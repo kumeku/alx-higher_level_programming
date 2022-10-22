760e511fcc351d7c27a1bfcfd1432d73cdab2c01
kumeku <101403261+kumeku@users.noreply.github.com>
2022-10-22 05:45:35 +0100

    # 0x11. Python -Network 1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
-"""Fetches a URL."""
-import requests
-
-
-if __name__ == '__main__':
-    response = requests.get('https://intranet.hbtn.io/status')
-    print('Body response:')
-    print('\t- type: {}'.format(type(response.text)))
-    print('\t- content: {}'.format(response.text))
+# Python script that fetches a url using only requests
+if __name__ == "__main__":
+    import requests as req
+    r = req.get('https://intranet.hbtn.io/status')
+    print('Body response:\n\t- type: {}\n\t- content: {}'
+          .format(type(r.text), r.text))
     
