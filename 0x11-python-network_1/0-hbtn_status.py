13959f1bf04659eeb8213bdaef4dc03613e4d51f
kumeku <101403261+kumeku@users.noreply.github.com>
2022-10-22 05:42:54 +0100

    # 0x11. Python -Network 1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
-"""Fetches a URL."""
-import urllib.request as request
+# Python script that fetches a url
+import urllib.request as ur
 
+with ur.urlopen('https://intranet.hbtn.io/status') as res:
+    res = res.read()
 
-if __name__ == "__main__":
-    with request.urlopen("https://intranet.hbtn.io/status") as response:
-        if response.readable():
-            data = response.read()
-            print("Body response:")
-            print("\t- type: {}".format(type(data)))
-            print("\t- content: {}".format(data))
-            print("\t- utf8 content: {}".format(data.decode("utf-8")))
+print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(res), res))
+print('\t- utf8 content: {}'.format(str(res, 'utf-8')))
